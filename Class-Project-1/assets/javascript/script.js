324c6ca7732576d67177ca034483c9dbc34c32ba
emiliohe1216 <emhdz92@gmail.com>
2025-03-07 22:23:36 -0500

    commit

--- Class-Project-1/assets/javascript/script.js
+++ Class-Project-1/assets/javascript/script.js
@@ -1,55 +1,53 @@
 
 
 
-const weatherKey = '4d57bc32eee398fdfe779489a997ae6c'
+
+const footballKey = 'b6e34e3420441ef09c2b786b23b70a9f'
+const teamNameInput = document.getElementById('team-name')
+const searchButton = document.getElementById('searchButton')
+
+
 
 //Spotify API//
 
+
 //API-FOOTBALL//
 
-//Team Info//
-fetch("https://v3.football.api-sports.io/teams?id=33", {
+//Team Selector//
+
+searchButton.addEventListener('click', (event) =>{
+	event.preventDefault();
+let teamName = teamNameInput.value; 
+
+fetch(`https://v3.football.api-sports.io/teams?name=${teamName}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "v3.football.api-sports.io",
-		"x-rapidapi-key": "b6e34e3420441ef09c2b786b23b70a9f"
+		"x-rapidapi-key": footballKey
 	}
-   })
- .then(response => {
-	console.log(response);
-    return response.json();
 })
-.then(data=> {
-    console.log(data);
+.then(response => response.json())
+.then(data => {
+	console.log(data);
+	displayTeamData(data);
 })
 .catch(err => {
 	console.log(err);
+})
+
 });
 
-//Weather API, City & Country Code//
-async function fetchCurrentWeather(city) {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherKeyKey}`
-    );
-    // Throw error if response is not successful
-    if (!response.ok) throw new Error('City not found');
-    return response.json();}
+function displayTeamData (data) {
+	const clubName = data.response[0].team.name;
+	const teamCountry = data.response[0].team.country;
+	const teamLogo = data.response[0].team.logo;
 
-	function displayCurrentWeather(data) {
-		// Create HTML template with current weather data
-		currentWeather.innerHTML = `
-			<div class="weather-card">
-				<h2>
-					${data.name} (${new Date().toLocaleDateString()})
-					<img class="weather-icon" src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" 
-						 alt="${data.weather[0].description}">
-				</h2>
-				<ul class="weather-info">
-					<li>Temperature: ${Math.round(data.main.temp)}°F</li>
-					<li>Humidity: ${data.main.humidity}%</li>
-					<li>Wind Speed: ${data.wind.speed} MPH</li>
-				</ul>
-			</div>
-		`;
-	}
-	console.log(fetchCurrentWeather)
+	const teamNameElement = document.querySelector('#team-card .card-title');
+	const teamCountryElement = document.querySelector('#team-card .card-text');
+	const teamLogoElement = document.querySelector('#team-logo');
+
+	teamNameElement.textContent = clubName;
+	teamCountryElement.textContent = teamCountry;
+	teamLogoElement.src = teamLogo;
+}
+
